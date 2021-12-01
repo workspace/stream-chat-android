652cb194fcfb826096301381fa9403fe94f0808b
Leandro Borges Ferreira <lehen01@gmail.com>
2021-12-01 15:20:14 -0300

    Using default filters, sort and limit for UsersGroupFragment

--- trelltest/src/main/java/io/getstream/trellsample/UsersGroupFragment.kt
+++ trelltest/src/main/java/io/getstream/trellsample/UsersGroupFragment.kt
@@ -1,22 +1,5 @@
 package io.getstream.trellsample
 
-import io.getstream.chat.android.client.ChatClient
-import io.getstream.chat.android.client.api.models.FilterObject
-import io.getstream.chat.android.client.api.models.QuerySort
-import io.getstream.chat.android.client.models.Channel
-import io.getstream.chat.android.client.models.Filters
 import io.getstream.chat.android.ui.channel.ChannelListFragment
 
-class UsersGroupFragment : ChannelListFragment() {
-    override fun getFilter(): FilterObject {
-        return Filters.`in`("members", listOf(ChatClient.instance().getCurrentUser()!!.id))
-    }
-
-    override fun getSort(): QuerySort<Channel> {
-        return QuerySort.desc(Channel::lastMessageAt)
-    }
-
-    override fun getLimit(): Int {
-        return 10
-    }
-}
+class UsersGroupFragment : ChannelListFragment()
