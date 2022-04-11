fd98bdb11442c486d4148cf1e6063f649dd0eafb
kanat <kijalbaev@gmail.com>
2022-04-11 10:43:18 +0400

    [13] support inline logging

--- stream-chat-android-offline/src/main/java/io/getstream/chat/android/offline/event/handler/internal/EventHandlerImpl.kt
+++ stream-chat-android-offline/src/main/java/io/getstream/chat/android/offline/event/handler/internal/EventHandlerImpl.kt
@@ -86,12 +86,16 @@ import io.getstream.chat.android.offline.plugin.state.StateRegistry
 import io.getstream.chat.android.offline.plugin.state.global.internal.GlobalMutableState
 import io.getstream.chat.android.offline.repository.builder.internal.RepositoryFacade
 import io.getstream.chat.android.offline.sync.internal.SyncManager
-import io.getstream.logging.StreamLog
+import io.getstream.logging.logD
+import io.getstream.logging.logI
+import io.getstream.logging.logV
 import kotlinx.coroutines.CoroutineScope
 import kotlinx.coroutines.Deferred
 import kotlinx.coroutines.async
 import java.util.Date
 import java.util.InputMismatchException
+
+private const val TAG = "EventHandler"
 
 internal class EventHandlerImpl(
     private val recoveryEnabled: Boolean,
@@ -102,7 +106,6 @@ internal class EventHandlerImpl(
     private val repos: RepositoryFacade,
     private val syncManager: SyncManager,
 ) {
-    private val logger = StreamLog.getLogger("EventHandler")
 
     private var eventSubscription: Disposable = EMPTY_DISPOSABLE
     private var initJob: Deferred<*>? = null
@@ -141,7 +144,7 @@ internal class EventHandlerImpl(
      */
     @VisibleForTesting
     internal suspend fun handleEvent(event: ChatEvent) {
-        logger.i { "[handleEvent] event: $event" }
+        logI(TAG) { "[handleEvent] event: $event" }
         handleConnectEvents(listOf(event))
         handleEventsInternal(listOf(event), isFromSync = false)
     }
@@ -158,7 +161,7 @@ internal class EventHandlerImpl(
     }
 
     private suspend fun replayEventsForChannels(cids: List<String>): Result<List<ChatEvent>> {
-        logger.i { "[replayEventsForChannels] cids: $cids" }
+        logI(TAG) { "[replayEventsForChannels] cids: $cids" }
         return queryEvents(cids)
             .onSuccessSuspend { eventList ->
                 handleEventsInternal(eventList, isFromSync = true)
@@ -174,7 +177,7 @@ internal class EventHandlerImpl(
     }
 
     private suspend fun handleEvents(events: List<ChatEvent>) {
-        logger.i { "[handleEvents] events.size: ${events.size}" }
+        logI(TAG) { "[handleEvents] events.size: ${events.size}" }
         handleConnectEvents(events)
         handleEventsInternal(events, isFromSync = false)
     }
@@ -182,13 +185,13 @@ internal class EventHandlerImpl(
     private suspend fun handleConnectEvents(sortedEvents: List<ChatEvent>) {
         // send out the connect events
         sortedEvents.forEach { event ->
+            logI(TAG) { "[handleConnectEvents] event: $event" }
             // connection events are never send on the recovery endpoint, so handle them 1 by 1
             when (event) {
                 is DisconnectedEvent -> {
                     mutableGlobalState._connectionState.value = ConnectionState.OFFLINE
                 }
                 is ConnectedEvent -> {
-                    logger.i { "[handleConnectEvents] Received ConnectedEvent, marking the domain as online and initialized" }
                     updateCurrentUser(event.me)
 
                     mutableGlobalState._connectionState.value = ConnectionState.CONNECTED
@@ -544,7 +547,7 @@ internal class EventHandlerImpl(
 
     private suspend fun handleEventsInternal(events: List<ChatEvent>, isFromSync: Boolean) {
         events.forEach { chatEvent ->
-            logger.v { "[handleEventsInternal] chatEvent: $chatEvent" }
+            logV(TAG) { "[handleEventsInternal] chatEvent: $chatEvent" }
         }
 
         val sortedEvents = events.sortedBy { it.createdAt }
@@ -641,7 +644,7 @@ internal class EventHandlerImpl(
             if (channel?.ownCapabilities?.contains(ChannelCapabilities.READ_EVENTS) == true) {
                 true
             } else {
-                logger.d {
+                logD(TAG) {
                     "Skipping unread counts update for channel: $cid. ${ChannelCapabilities.READ_EVENTS} capability is missing."
                 }
                 false
