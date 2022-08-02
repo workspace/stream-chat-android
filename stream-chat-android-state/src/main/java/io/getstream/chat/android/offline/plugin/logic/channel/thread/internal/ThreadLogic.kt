8f7a8beadb28b2f0b1cb4473e89752e99a37d238
Kimin Ryu <ryu@holix.com>
2022-08-02 20:20:03 +0900

    [ISSUE-3956] Insert new messsages into database in ThreadLogic and setLoading when onGetRepliesRequest invoked

--- stream-chat-android-state/src/main/java/io/getstream/chat/android/offline/plugin/logic/channel/thread/internal/ThreadLogic.kt
+++ stream-chat-android-state/src/main/java/io/getstream/chat/android/offline/plugin/logic/channel/thread/internal/ThreadLogic.kt
@@ -27,13 +27,16 @@ import io.getstream.chat.android.client.events.ReactionDeletedEvent
 import io.getstream.chat.android.client.events.ReactionNewEvent
 import io.getstream.chat.android.client.events.ReactionUpdateEvent
 import io.getstream.chat.android.client.models.Message
+import io.getstream.chat.android.client.persistance.repository.RepositoryFacade
 import io.getstream.chat.android.client.plugin.listeners.ThreadQueryListener
 import io.getstream.chat.android.client.utils.Result
+import io.getstream.chat.android.client.utils.onSuccessSuspend
 import io.getstream.chat.android.offline.plugin.state.channel.thread.internal.ThreadMutableState
 import io.getstream.logging.StreamLog
 
 /** Logic class for thread state management. Implements [ThreadQueryListener] as listener for LLC requests. */
 internal class ThreadLogic(
+    private val repos: RepositoryFacade,
     private val client: ChatClient,
     private val threadStateLogic: ThreadStateLogic,
 ) : ThreadQueryListener {
@@ -41,28 +44,9 @@ internal class ThreadLogic(
     private val mutableState: ThreadMutableState = threadStateLogic.writeThreadState()
     private val logger = StreamLog.getLogger("Chat:ThreadLogic")
 
-    /** Runs precondition for loading more messages for thread. */
-    private fun precondition(): Result<Unit> {
-        return if (mutableState.loadingOlderMessages.value) {
-            val errorMsg = "already loading messages for this thread, ignoring the load more requests."
-            logger.i { errorMsg }
-            Result(ChatError(errorMsg))
-        } else {
-            Result.success(Unit)
-        }
-    }
-
-    /** Runs side effect when a request is going to be launched. */
-    @Suppress("UnusedPrivateMember")
-    private fun onRequest() {
-        mutableState.setLoadingOlderMessages(true)
-        // TODO implement thread replies loading from DB
-    }
-
     /** Runs side effect when a result is obtained. */
-    private fun onResult(result: Result<List<Message>>, limit: Int) {
+    private suspend fun onResult(result: Result<List<Message>>, limit: Int) {
         if (result.isSuccess) {
-            // Note that we don't handle offline storage for threads at the moment.
             val newMessages = result.data()
             upsertMessages(newMessages)
             mutableState.setEndOfOlderMessages(newMessages.size < limit)
@@ -72,41 +56,68 @@ internal class ThreadLogic(
                     ?: mutableState.oldestInThread.value
             )
         }
-        mutableState.setLoadingOlderMessages(false)
-    }
-
-    override suspend fun onGetRepliesPrecondition(messageId: String, limit: Int): Result<Unit> {
-        val result = client.getMessage(messageId).await()
-        return if (result.isSuccess) {
-            upsertMessages(listOf(result.data()))
-            Result.success(Unit)
-        } else {
-            Result(result.error())
+        result.onSuccessSuspend {
+            repos.insertMessages(it)
         }
     }
 
-    override suspend fun onGetRepliesRequest(messageId: String, limit: Int) = onRequest()
+    override suspend fun onGetRepliesPrecondition(messageId: String, limit: Int): Result<Unit> {
+        return if (mutableState.loading.value) {
+            val errorMsg = "already loading messages for this thread, ignoring the load requests."
+            logger.i { errorMsg }
+            Result(ChatError(errorMsg))
+        } else {
+            val result = client.getMessage(messageId).await()
+            if (result.isSuccess) {
+                upsertMessage(result.data())
+                repos.insertMessage(result.data())
+                Result.success(Unit)
+            } else {
+                Result(result.error())
+            }
+        }
+    }
 
-    override suspend fun onGetRepliesResult(result: Result<List<Message>>, messageId: String, limit: Int) =
+    override suspend fun onGetRepliesRequest(messageId: String, limit: Int) {
+        mutableState.setLoading(true)
+    }
+
+    override suspend fun onGetRepliesResult(result: Result<List<Message>>, messageId: String, limit: Int) {
+        mutableState.setLoading(false)
         onResult(result, limit)
+    }
 
-    override suspend fun onGetRepliesMorePrecondition(messageId: String, firstId: String, limit: Int) = precondition()
+    override suspend fun onGetRepliesMorePrecondition(messageId: String, firstId: String, limit: Int): Result<Unit> {
+        return if (mutableState.loadingOlderMessages.value) {
+            val errorMsg = "already loading messages for this thread, ignoring the load more requests."
+            logger.i { errorMsg }
+            Result(ChatError(errorMsg))
+        } else {
+            Result.success(Unit)
+        }
+    }
 
     override suspend fun onGetRepliesMoreRequest(
         messageId: String,
         firstId: String,
         limit: Int
-    ) = onRequest()
+    ) {
+        mutableState.setLoadingOlderMessages(true)
+    }
 
     override suspend fun onGetRepliesMoreResult(
         result: Result<List<Message>>,
         messageId: String,
         firstId: String,
         limit: Int
-    ) = onResult(result, limit)
+    ) {
+        mutableState.setLoadingOlderMessages(false)
+        onResult(result, limit)
+    }
 
     internal fun upsertMessage(message: Message) = upsertMessages(listOf(message))
-    internal fun upsertMessages(messages: List<Message>) = threadStateLogic.upsertMessages(messages)
+
+    private fun upsertMessages(messages: List<Message>) = threadStateLogic.upsertMessages(messages)
 
     internal fun handleEvents(events: List<HasMessage>) {
         for (event in events) {
